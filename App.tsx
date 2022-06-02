9ec03329ad73cfeac353c2712e5afec132adfbc1
jd <>
2022-06-02 16:06:55 +0900

    react-query 초기화 (QueryClientProvider 래핑)

--- App.tsx
+++ App.tsx
@@ -10,13 +10,18 @@
 
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
+import {QueryClient, QueryClientProvider} from 'react-query';
 import BottomTabs from './navigations/BottomTabs';
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <BottomTabs />
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <BottomTabs />
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 };
 
