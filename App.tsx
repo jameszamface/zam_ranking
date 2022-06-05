e47d611960cd8572ff64b59330af97cdf8f36290
jd <>
2022-06-05 17:06:41 +0900

    [FIX] SafeAreaView로 래핑

--- App.tsx
+++ App.tsx
@@ -13,17 +13,26 @@ import React from 'react';
 import {QueryClient, QueryClientProvider} from 'react-query';
 import BottomTabs from './navigations/BottomTabs';
 import 'react-native-gesture-handler';
+import {SafeAreaView, StyleSheet} from 'react-native';
 
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <NavigationContainer>
-        <BottomTabs />
-      </NavigationContainer>
-    </QueryClientProvider>
+    <SafeAreaView style={styles.container}>
+      <QueryClientProvider client={queryClient}>
+        <NavigationContainer>
+          <BottomTabs />
+        </NavigationContainer>
+      </QueryClientProvider>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
