da5746e3c829494f155426843dbb8d007d223777
jd <>
2022-06-06 12:54:57 +0900

    [FIX] ScrollViewWithScrollTo에서 Reanimated.ScrollView를 ScrollView로 교체

--- components/ScrollViewWithScrollTo.tsx
+++ components/ScrollViewWithScrollTo.tsx
@@ -1,48 +1,20 @@
-import React, {useCallback} from 'react';
-import {
-  NativeScrollEvent,
-  NativeSyntheticEvent,
-  ScrollViewProps,
-} from 'react-native';
-import Animated, {
-  useAnimatedRef,
-  useDerivedValue,
-  useSharedValue,
-  scrollTo as reanimatedScrollTo,
-  withDelay,
-  withTiming,
-  AnimateProps,
-} from 'react-native-reanimated';
+import React, {useCallback, useRef} from 'react';
+import {ScrollView, ScrollViewProps} from 'react-native';
 
 export type ScrollTo = (
   offset: {x: number} | {y: number},
   options?: {delay?: number; duration?: number},
 ) => void;
 
-const ScrollViewWithScrollTo = ({
-  children,
-  ...props
-}: AnimateProps<ScrollViewProps>) => {
-  const ref = useAnimatedRef<Animated.ScrollView>();
-  const scrollX = useSharedValue(0);
-  const scrollY = useSharedValue(0);
-
-  useDerivedValue(() => {
-    reanimatedScrollTo(ref, scrollX.value, scrollY.value, false);
-  }, [scrollX, scrollY]);
+const ScrollViewWithScrollTo = ({children, ...props}: ScrollViewProps) => {
+  const ref = useRef<ScrollView>(null);
 
   const scrollTo: ScrollTo = useCallback(
     (offset, options?: {delay?: number; duration?: number}) => {
-      const scroll = 'x' in offset ? scrollX : scrollY;
-      const value = 'x' in offset ? offset.x : offset.y;
-      scroll.value = withDelay(
-        options?.delay || 0,
-        withTiming(value, {
-          duration: options?.duration,
-        }),
-      );
+      setTimeout(() => {
+        ref.current?.scrollTo(offset);
+      }, options?.duration || 0);
     },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
     [],
   );
 
@@ -53,28 +25,10 @@ const ScrollViewWithScrollTo = ({
     return child;
   });
 
-  const onScrollEnd = useCallback(
-    ({
-      nativeEvent: {
-        contentOffset: {x, y},
-      },
-    }: NativeSyntheticEvent<NativeScrollEvent>) => {
-      scrollX.value = x;
-      scrollY.value = y;
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [],
-  );
-
   return (
-    <Animated.ScrollView
-      scrollEventThrottle={25}
-      onScrollEndDrag={onScrollEnd}
-      onMomentumScrollEnd={onScrollEnd}
-      ref={ref}
-      {...props}>
+    <ScrollView scrollEventThrottle={25} ref={ref} {...props}>
       {childrenWithProps}
-    </Animated.ScrollView>
+    </ScrollView>
   );
 };
 
