09952fa986165330c27f796636e33f995b563c26
jd <>
2022-06-08 14:17:54 +0900

    [FEAT] CircleImage 추가

---
+++ components/CircleImage.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import FastImage from 'react-native-fast-image';
+import styled from 'styled-components/native';
+
+interface Props {
+  size: number;
+  image: string;
+}
+
+function CircleImage({size, image}: Props) {
+  return <Image size={size} source={{uri: image}} />;
+}
+
+const Image = styled(FastImage)<{size: number}>`
+  width: ${props => props.size}px;
+  height: ${props => props.size}px;
+  border-radius: ${props => props.size / 2}px;
+`;
+
+export default CircleImage;
