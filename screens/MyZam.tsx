826f15183d19d9e35dbe84232e0e9d10e7ff8ea6
jd <>
2022-06-08 14:03:03 +0900

    [FEAT] @monterosa/react-native-parallax-scroll 설치, Parallax 스크롤 프레임 구성

--- screens/MyZam.tsx
+++ screens/MyZam.tsx
@@ -1,12 +1,74 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {ScrollView, FlatList, Text, View, Animated} from 'react-native';
 import styled from 'styled-components/native';
+import ParallaxScrolView from '@monterosa/react-native-parallax-scroll';
+import {width} from '../constants';
+
+const profileHeight = width;
 
 function MyZam() {
   return (
-    <Container>
-      <Text>My Zam</Text>
-    </Container>
+    <FlatList
+      style={{flex: 1}}
+      ListHeaderComponent={
+        <View
+          style={{
+            width: '100%',
+            height: 450,
+            backgroundColor: 'yellow',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text>List Header</Text>
+        </View>
+      }
+      renderScrollComponent={() => (
+        <ParallaxScrolView
+          headerHeight={0}
+          isHeaderFixed={false}
+          parallaxHeight={profileHeight}
+          fadeOutParallaxBackground
+          renderParallaxBackground={({
+            animatedValue, // [0~headerHeight]
+          }: {
+            animatedValue: Animated.Value;
+          }) => {
+            const opacity = animatedValue.interpolate({
+              inputRange: [0, profileHeight],
+              outputRange: [0, 1],
+            });
+            return (
+              <Animated.View
+                style={{
+                  backgroundColor: '#ffffff',
+                  position: 'absolute',
+                  opacity,
+                  height: profileHeight,
+                  width: '100%',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}>
+                <Text style={{color: '#000000'}}>Parallax Background</Text>
+              </Animated.View>
+            );
+          }}
+          parallaxBackgroundScrollSpeed={5}
+          parallaxForegroundScrollSpeed={5}
+        />
+      )}
+      data={[1, 2, 3, 4, 5, 6]}
+      bounces
+      renderItem={() => (
+        <View
+          style={{
+            height: 150,
+            width: '100%',
+            backgroundColor: 'blue',
+            opacity: 0.5,
+          }}
+        />
+      )}
+    />
   );
 }
 
