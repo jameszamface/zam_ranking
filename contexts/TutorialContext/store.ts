ea0564434ca4ddd0e427e99b645f53da0723489e
jd <>
2022-06-16 13:58:33 +0900

    [FEAT] saveCompletedTutorialIds, restoreCompletedTutorialIds 추가

---
+++ contexts/TutorialContext/store.ts
@@ -0,0 +1,28 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const key = '@completed_tutorials_key';
+
+export async function restoreCompletedTutorialIds() {
+  return JSON.parse((await AsyncStorage.getItem(key)) || '[]') as number[];
+}
+
+export async function saveCompletedTutorialId(id: number) {
+  const completedTutorialIds = await restoreCompletedTutorialIds();
+
+  // 중복 제거
+  const newCompletedTutorialIds = addItemWithoutDuplications(
+    completedTutorialIds,
+    id,
+  );
+
+  AsyncStorage.setItem(key, JSON.stringify(newCompletedTutorialIds));
+}
+
+const addItemWithoutDuplications = <T extends string | number>(
+  array: Array<T>,
+  item: T,
+) => {
+  const set = new Set(array);
+  set.add(item);
+  return [...set];
+};
