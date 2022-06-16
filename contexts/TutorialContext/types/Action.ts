324afc22e479117994ef9ea81db1e6cc68465ec2
jd <>
2022-06-16 11:12:45 +0900

    [FEAT] Action > Modal 타입 추가 [FIX] 공통 타입을 common으로 이전

--- contexts/TutorialContext/types/Action.ts
+++ contexts/TutorialContext/types/Action.ts
@@ -1,13 +1,27 @@
-import {State} from '.';
+import {Button, Image, Position, Size, State, Text} from './common';
 
 export enum ActionType {
-  Auto = 0, // 트리거되고 duration 이후, state가 완료 상태로 변경됩니다.
+  // 트리거되고 duration 이후, state가 완료 상태로 변경됩니다.
+  // modal.button이 존재하는 경우, 무조건 Manual로 동작합니다.
+  Auto = 0,
   Manual, // children에서 직접 완료 신호를 주어야 state가 완료 상태로 변경됩니다.
 }
 
+// 2.22 기획서 p.14에 튜토리얼 이탈시 처음부터 다시 시작한다고 했으므로, state가 State.Pending인 튜토리얼은 state가 State.Complete인 Action도 다시 트리거시킵니다.
 export interface Action {
   id: number;
   type: ActionType;
   state: State;
-  duration: number; // millisecond
+  duration: number; // millisecond 단위이고, modal.button이 존재하는 경우 무시됩니다.
+  modal: Modal;
+  image?: Image;
+}
+
+export interface Modal {
+  size: Size;
+  position: Position;
+  topImage?: Image; // 텍스트 상단에 표시되는 이미지입니다. 이미지 사이즈는 모달 사이즈를 기준으로 합니다.
+  text: Text;
+  bottomImage?: Image; // 텍스트 하단에 표시되는 이미지입니다.
+  button?: Button;
 }
---
+++ contexts/TutorialContext/types/common.ts
@@ -0,0 +1,54 @@
+import { StyleProp, TextStyle, ViewStyle } from "react-native";
+
+export enum State {
+  Pending = 0, // 트리거 전
+  Complete,
+}
+
+// width와 height는 비율(0~1) 또는 절대값(1~number)을 값으로 가질 수 있습니다.
+export interface Size {
+  width: number;
+  height: number;
+}
+
+// 각 position 요소는 비율(0~1) 또는 절대값(1~number)을 값으로 가질 수 있습니다.
+// top과 bottom이 동시에 입력되면 top이 우선합니다.
+// left와 right가 동시에 입력되면 right가 우선합니다.
+// top과 bottom 또는 left와 right가 모두 입력되지 않으면 상하/좌우 가운데 정렬됩니다.
+// 엘리먼트의 좌상단이 position에 위치하게 됩니다.
+export interface Position {
+  top?: number;
+  bottom?: number;
+  left?: number;
+  right?: number;
+}
+
+export type Text =
+  | string
+  | (
+      | string
+      | {
+          color: string;
+          text: string;
+        }
+    )[];
+
+// image: jpeg, png, gif 등 Image 컴포넌트로 랜더링할 수 있는 이미지입니다.
+// svg: react-native-svg 라이브러리로 랜더링할 수 있는 이미지입니다.
+// lottie: lottie-react-native 라이브러리로 랜더링할 수 있는 이미지입니다.
+export type ImageType = 'image' | 'svg' | 'lottie';
+
+// 모달 안에서 표시되는 이미지와 모달과 같은 레벨로 모달보다 높은 z-index로 표시되는 이미지가 있을 수 있습니다.
+export interface Image {
+  size: Size;
+  position: Position;
+  uri: string;
+  type: ImageType;
+  duration: number; // millisecond
+}
+
+export interface Button {
+  text: string;
+  containerStyle: StyleProp<ViewStyle>;
+  textString: StyleProp<TextStyle>;
+}
